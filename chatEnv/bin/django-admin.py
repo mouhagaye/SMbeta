3d1beac667ab2af635ff7e9e5f0c32fd6fe20087
Mouhamadou Gaye <mouhagaye2015@gmail.com>
2019-07-18 16:50:56 +0000

    moved env

--- chatEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/mouhagaye/Documents/Trans/django-eventstream/examples/chat/chatEnv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
